3a3f143c833f116e722d1ad03f6625ff98dc7ea2
ModernType <antonaw2004@gmail.com>
2025-12-04 01:05:05 +0200

    Added message server rebind

--- src/accept_server.rs
+++ src/accept_server.rs
@@ -1,6 +1,6 @@
 //! This module has all functionality to serve a message awaiting server
 
-use std::{net::SocketAddrV4, sync::OnceLock};
+use std::{net::SocketAddrV4, sync::OnceLock, time::Duration};
 
 use crate::{app_state::APP_STATE, SendMode, message::OperatorMessage as Message, signal_actions::SignalAction};
 use axum::{Router, http::StatusCode, response::IntoResponse, routing::post};
@@ -87,7 +87,8 @@ pub fn start_server_thread(
     app_handle: Weak<crate::App>,
 ) -> std::thread::JoinHandle<()> {
     std::thread::spawn(|| {
-        HANDLES.get_or_init(move || HandleContainer::new(action_send, app_handle));
+        let app_handle2 = app_handle.clone();
+        HANDLES.get_or_init(move || HandleContainer::new(action_send, app_handle2));
 
         let rt = tokio::runtime::Builder::new_current_thread()
             .enable_all()
@@ -99,7 +100,7 @@ pub fn start_server_thread(
         };
 
         rt.block_on(async move {
-            match start_server(listener_addr).await {
+            match start_server(listener_addr, app_handle).await {
                 Ok(_) => info!("Message server finished successfuly"),
                 Err(e) => panic!("Message server error: {e}"),
             }
@@ -107,9 +108,24 @@ pub fn start_server_thread(
     })
 }
 
-async fn start_server(addr: SocketAddrV4) -> anyhow::Result<()> {
+async fn start_server(addr: SocketAddrV4, app_handle: Weak<crate::App>) -> anyhow::Result<()> {
     info!("Binding on addr {}", &addr);
-    let listener = TcpListener::bind(addr).await?;
+    let listener = loop {
+        match TcpListener::bind(addr).await {
+            Ok(listener) => {
+                _ = app_handle.upgrade_in_event_loop(move |app| {
+                    app.invoke_report("Сервер для отримання повідомлень запущено".into());
+                });
+                break listener
+            },
+            Err(_e) => {
+                _ = app_handle.upgrade_in_event_loop(move |app| {
+                    app.invoke_report(slint::format!("Не можу запустити сервер на адресі {}. Може вона вже зайнята. Змініть її у налаштуваннях", &addr));
+                });
+                tokio::time::sleep(Duration::from_secs(5)).await;
+            }
+        }
+    };
 
     let router = Router::new().route("/", post(message_post));
 
